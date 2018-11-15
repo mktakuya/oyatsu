f2fb2f6c6cb2df8d8c9af9f4276bb2d10a4cb6b6
Takuya Mukohira <takuya.mk96@gmail.com>
2018-11-15 15:09:48 +0900

    Implement users#create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate!, only: [ :sign_in ]
+
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @uer
+    else
+      render json: { errors: @user.errors.full_messages }
+    end
   end
 
   def sign_in
@@ -13,5 +22,15 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def me
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :email,
+      :password,
+      :name
+    )
   end
 end
