8f8454f5a66ae4dd1a7e4bcbf5cbc2e5112631fe
Takuya Mukohira <takuya.mk96@gmail.com>
2018-11-15 14:30:33 +0900

    Implement users#sign_in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,13 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def sign_in
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      render json: @user
+    else
+      render json: { errors: ['ログインに失敗しました'] }, status: 401
+    end
   end
 
   def me
