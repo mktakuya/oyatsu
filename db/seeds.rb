75c53c47131e22b9a5065d069b2d60064b04ac7f
Takuya Mukohira <takuya.mk96@gmail.com>
2018-11-18 23:21:31 +0900

    Add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  User.find_or_create_by(email: 'takuya.mk96@gmail.com') do |user|
+    user.password = 'password'
+    user.name = 'mktakuya'
+    user.token = 'GkueWCnUSiEXZD8E9KEoc4TF' # Sample
+  end
+end
