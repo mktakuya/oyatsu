a3b60ace30b03840af4360f690ef7c0cf61b9d6e
Takuya Mukohira <takuya.mk96@gmail.com>
2018-11-19 11:27:06 +0900

    Set default user password if password is blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token
 
+  before_validation :set_default_password, on: :create, if: Proc.new { |user| user.password_digest.blank? }
+
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :name, presence: true
@@ -9,4 +11,10 @@ class User < ApplicationRecord
   validates :token, uniqueness: true
 
   has_many :items
+
+  private
+
+  def set_default_password
+    self.password = email.split('@')[0].reverse
+  end
 end
